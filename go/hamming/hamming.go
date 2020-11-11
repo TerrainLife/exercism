31f7a19ad530d7b10e4b27a471e69a56e6bfc826
TerrainLife <embedded.life@yandex.ru>
2020-11-11 22:38:08 +0300

    [Golang] Hamming solution

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,26 @@
+/*
+Package hamming implement Hamming exercise solution in https://exercism.io/
+*/
 package hamming
 
+import (
+	"errors"
+)
+
+// Distance calc Hamming Distance between two DNA strands
 func Distance(a, b string) (int, error) {
+	if (a == "" && b != "") || (a != "" && b == "") {
+		return 0, errors.New("Error - empty stand(s)!")
+	}
+	if len(a) != len(b) {
+		return 0, errors.New("Error - stands have different len!")
+	}
+
+	difs := 0
+	for i := range a {
+		if a[i] != b[i] {
+			difs++
+		}
+	}
+	return difs, nil
 }
